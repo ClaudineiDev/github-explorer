5598bbc1f923dca51f7d2d194e8c23b963074684
ClaudineiDev <1088claudinei@gmail.com>
2021-05-10 14:03:01 -0300

    add useState, useEffect e http

--- reactjs/github-explorer/src/components/RepositoryList.jsx
+++ reactjs/github-explorer/src/components/RepositoryList.jsx
@@ -1,20 +1,26 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../styles/repositories.scss';
+import { useEffect, useState } from "react";
+
 export function RepositoryList() {
-    const repository = {
-        name: 'unform#',
-        description: 'Forms in React',
-        link: 'http://github.com/unform/unform'
-    }
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(()=> {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
     return(
         <section className= "repository-list">
             <h1>Lista de Repositorios</h1>
 
             <ul>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem/>
-                <RepositoryItem/>
-                <RepositoryItem/>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
+                
             </ul>
 
         </section>
